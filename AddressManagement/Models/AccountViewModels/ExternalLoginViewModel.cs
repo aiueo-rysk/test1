c7a4bcdfb07de3698fe0dd8d3f8be26c1dd18770
miyamoto <www.aiueo.r.m@gmail.com>
2019-01-02 14:25:29 +0900

    外部サービス認証の仕組みを変更
    追加したファイルのコミット

---
+++ AddressManagement/Models/AccountViewModels/ExternalLoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressManagement.Models.AccountViewModels
+{
+    public class ExternalLoginViewModel
+    {
+        [Required(ErrorMessage = "{0}は必須入力です。")]
+        [EmailAddress(ErrorMessage = "{0}の形式が不正です。")]
+        [Display(Name = "メールアドレス")]
+        public string Email { get; set; }
+    }
+}
